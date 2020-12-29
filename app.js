e8ed10945c015dfeced8337535d92743bdf30be9
Rizza Magana <codingwithrich@gmail.com>
2020-12-29 12:11:37 +0800

    installed helmet and compression

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
+var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
+var compression = require('compression');
+var helmet = require('helmet');
+
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
+const compression = require('compression');
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(compression());
+app.use(helmet());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', routes);
 app.use(express.static('public'));
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 module.exports = app;
