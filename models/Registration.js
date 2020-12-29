f902572055477f0d3e518da37a49ee28ebfd2d6b
Rizza Magana <codingwithrich@gmail.com>
2020-12-29 14:09:34 +0800

    updated schema name

--- models/Registration.js
+++ models/Registration.js
@@ -11,4 +11,4 @@ const registrationSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model('Registration', registrationSchema);
+module.exports = mongoose.model('registration', registrationSchema);
