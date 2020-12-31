4e056c007a8869e70f37acf92a14ed4cda678a70
Rizza Magana <codingwithrich@gmail.com>
2020-12-31 15:45:47 +0800

    fixed table layout for /student-profile/

--- routes/controller.js
+++ routes/controller.js
@@ -12,57 +12,14 @@ const router = express.Router();
 
 module.exports = router;
 
-router.get('/', (req, res) => {
-    res.render('form', { title: 'Registration form' });
-});
-
-var validate = [
-    check('name').isLength({ min: 1 }).withMessage('Name cannot be blank.'),
-    check('email').isLength({ min: 1 }).withMessage('Email address cannot be blank.')
-]
-
-router.post('/', validate, (req, res) => {
-    processRegistrationForm(req, res);
-});
-
-function processRegistrationForm(req, res) {
-    console.log(req.body);
-    const errors = validationResult(req);
-
-    if (errors.isEmpty()) {
-        const registration = new Registration(req.body);
-        registration.save().then(() => {
-            res.send("Thank you! Bye.");
-        }).catch((err) => {
-            console.log(err);
-            res.send('Sorry! Something went wrong.')
-        });
-    } else {
-        res.render('form', {
-            title: 'Registration form',
-            errors: errors.array(),
-            data: req.body
-        });
-    }
+function redirectStudentProfileForm(req, res){
+    res.render('student-profile-form', {
+        title: 'Student Profile Form',
+        data: req.body
+    });
 }
 
-router.get('/registrations', basic.check((req, res) => {
-    getRegistrationList(req, res)
-}));
-
-function getRegistrationList(req, res) {
-    Registration.find()
-        .then((registrations) => {
-            res.render('index', { title: 'Listing registrations', registrations });
-        })
-        .catch(() => { res.send('Sorry! Something when wrong.') })
-}
-
-router.get('/student-profile/form', (req, res) => {
-    res.render('student-profile-form')
-});
-
-router.post('/student-profile/form', (req,res) => {
+function submitStudentProfileForm(req, res){
     console.log("Request Body:" + req.body);
     const errors = validationResult(req);
 
@@ -70,11 +27,7 @@ router.post('/student-profile/form', (req,res) => {
         const student = new StudentProfiles(req.body);
         console.log("Student:" + student);
         student.save().then(() => {
-            StudentProfiles.find()
-                .then((students) => {
-                    res.render('student-profiles', { title: 'Listing students', students });
-                })
-                .catch(() => { res.send('Sorry! Something when wrong.') })
+           getStudentProfiles(res);
         }).catch((err) => {
             console.log(err);
             res.send('Sorry! Something went wrong.')
@@ -85,16 +38,37 @@ router.post('/student-profile/form', (req,res) => {
             data: req.body 
         });
     }
+}
+
+function getStudentProfiles(res){
+    StudentProfiles.find()
+    .then((students) => {
+        res.render('student-profiles', { title: 'Student Profiles', students });
+    })
+    .catch(() => { res.send('Sorry! Something when wrong.') });
+}
+
+router.get('/', (req, res) => {
+    redirectStudentProfileForm(req, res);
+});
+
+router.post('/', (req, res) => {
+   submitStudentProfileForm(req, res);
+});
+
+router.get('/student-profile/form', (req, res) => {
+   redirectStudentProfileForm(req, res);
+});
+
+router.post('/student-profile/form', (req,res) => {
+   submitStudentProfileForm(req,res);
 });
 
 router.get('/student-profile', (req, res) => {
-    StudentProfiles.find()
-    .then((students) => {
-        res.render('student-profiles', { title: 'Listing students', students });
-    })
-    .catch(() => { res.send('Sorry! Something when wrong.') })
+    getStudentProfiles(res);
 });
 
-router.get('/form-elements', (req, res) => {
-    res.render('form-elements')
-});
+// var validate = [
+//     check('name').isLength({ min: 1 }).withMessage('Name cannot be blank.'),
+//     check('email').isLength({ min: 1 }).withMessage('Email address cannot be blank.')
+// ]
