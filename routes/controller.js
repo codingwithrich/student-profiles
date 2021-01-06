6717872f4472272eb84033cc8573daab0c3075f3
Rizza Magana <codingwithrich@gmail.com>
2021-01-07 01:12:20 +0800

    working register and login

--- routes/controller.js
+++ routes/controller.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const studentProfiles = mongoose.model('studentProfiles');
 const parentDetails = mongoose.model('studentParentDetails');
 const paymentDetails = mongoose.model('studentPaymentDetails');
+const users = mongoose.model('users');
 
 const { check, validationResult } = require('express-validator');
 
@@ -13,7 +14,7 @@ const router = express.Router();
 
 module.exports = router;
 
-function redirectStudentProfileForm(req, res){
+function renderStudentProfileForm(req, res){
     res.render('student-profile-form', {
         title: 'Student Profile Form'
     });
@@ -85,8 +86,54 @@ function renderStudentProfilePage(req, res){
     res.render('student-profiles', { title: 'Student Profiles', students: req.students, parents: req.parents, payments: req.payment });
 }
 
+function validateLogin(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    console.log('[Start] Validate Login for ' + username);
+
+    users.where({'username': username, 'password': password})
+    .findOne(function(err,  user){
+        if(err) {
+            console.log(err); res.send('Sorry! Something went wrong.');
+        } else if (user == null) {
+            res.render('login', { error: true });
+        } else {
+            console.log('User found! ' + user);
+            res.render('student-profile-form', {
+                title: 'Student Profile Form',
+                response: user
+            });;
+        }
+    });
+}
+
+function renderLogin(req, res){
+    res.render('login', { title: 'Login'});
+}
+
+function registerUser(req, res){
+    console.log("Request Body:" + req.body);
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+        const user = new users(req.body);
+        console.log("User:" + user);
+        user.save().then(() => {
+           renderLogin(req, res);
+        }).catch((err) => {
+            console.log(err);
+            res.send('Sorry! Something went wrong.')
+        });
+    } else {
+        res.render('register', {
+            errors: errors.array(),
+            data: req.body 
+        });
+    }
+}
+
 router.get('/', (req, res) => {
-    redirectStudentProfileForm(req, res);
+    renderStudentProfileForm(req, res);
 });
 
 router.post('/', (req, res) => {
@@ -94,7 +141,7 @@ router.post('/', (req, res) => {
 });
 
 router.get('/student-profile/form', (req, res) => {
-   redirectStudentProfileForm(req, res);
+   renderStudentProfileForm(res);
 });
 
 router.post('/student-profile/form', (req,res) => {
@@ -104,43 +151,42 @@ router.post('/student-profile/form', (req,res) => {
 router.get('/student-profile', getStudentProfiles, getParentDetailsByStudent, getPaymentDetailsByStudent, renderStudentProfilePage);
 
 router.get('/login', (req, res) =>{
-    res.render('login', { title: 'Login'});
+    renderLogin(req, res);
 });
 
-router.post('login', (req, res) => {
+router.post('/login', (req, res) => {
     console.log("Request Body:" + req.body);
     const errors = validationResult(req);
 
-    if (errors.isEmpty()) {
-        const student = new studentProfiles(req.body);
-        const student_username = student.first_name.split(" ") + student.last_name;
-        student.username = student_username;
-        console.log("Student:" + student);
-
-        const parents = new parentDetails(req.body);
-        parents.student_username = student_username;
-        console.log("Parents:" + parents);
-
-        const payment = new paymentDetails(req.body);
-        payment.student_username = student_username;
-        console.log("Payment: " + payment);
-
-        parents.save();
-        payment.save();
-        student.save().then(() => {
-           getStudentProfiles(res);
-        }).catch((err) => {
-            console.log(err);
-            res.send('Sorry! Something went wrong.')
-        });
+    if (errors.isEmpty()) { 
+        validateLogin(req, res);
     } else {
-        res.render('student-profile/form', {
+        res.render('login', {
+            title: 'Login',
             errors: errors.array(),
             data: req.body 
         });
     }
 });
 
+router.get('/register', (req, res) => {
+    res.render('register', { title: 'Register'})
+});
+
+router.post('/register', (req, res) => {
+    console.log("Request Body:" + req.body);
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+       registerUser(req, res);
+    } else {
+        res.render('register', {
+            title: 'Register',
+            errors: errors.array(),
+            data: req.body 
+        });
+    }
+});
 // var validate = [
 //     check('name').isLength({ min: 1 }).withMessage('Name cannot be blank.'),
 //     check('email').isLength({ min: 1 }).withMessage('Email address cannot be blank.')
--- start.js
+++ start.js
@@ -15,6 +15,7 @@ mongoose.connection
 });
 
 require('./models/studentprofiles');
+require('./models/users');
 const app = require('./app');
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Express is running on port ${server.address().port}`);
